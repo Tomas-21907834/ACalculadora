ca29a596e766ae6f7586d204d8d69df0c07dcdcc
tomas <tomasmartins2@gmail.com>
2022-03-25 10:22:45 +0000

    Initial commit

---
+++ app/src/main/java/com/example/acalculadora/MainActivity.kt
@@ -0,0 +1,50 @@
+package com.example.acalculadora
+
+import android.app.Activity
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import com.example.acalculadora.databinding.ActivityMainBinding
+import net.objecthunter.exp4j.Expression
+import net.objecthunter.exp4j.ExpressionBuilder
+
+class MainActivity : AppCompatActivity() {
+
+    private val TAG = MainActivity::class.java.simpleName
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+
+
+    override fun onStart() {
+        super.onStart()
+
+        binding.button1.setOnClickListener {
+            Log.i(TAG, "Click no botão 1")
+            if (binding.textVisor.text == "0") {
+                binding.textVisor.text = "1"
+            } else {
+                binding.textVisor.append("1")
+            }
+        }
+
+        binding.buttonAdition.setOnClickListener {
+            Log.i(TAG, "Click no botão +")
+            binding.textVisor.append("+")
+        }
+
+        binding.buttonEquals.setOnClickListener {
+            Log.i(TAG, "Click no botão =")
+            val expression = ExpressionBuilder(
+                binding.textVisor.text.toString()
+            ).build()
+            binding.textVisor.text = expression.evaluate().toString()
+            Log.i(TAG,"O resultado da expressão é ${binding.textVisor.text}")
+
+        }
+    }
+}
